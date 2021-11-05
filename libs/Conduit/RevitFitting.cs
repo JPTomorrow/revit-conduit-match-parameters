c4d189bb2fccbe60f65236769970ce5b9bd43ff6
Justin Morrow <justin.parker.morrow@gmail.com>
2021-11-05 10:01:14 -0500

    rebuilt program with modern systems and libraries

--- libs/Conduit/RevitFitting.cs
+++ libs/Conduit/RevitFitting.cs
@@ -15,14 +15,14 @@ using JPMorrow.Revit.Measurements;
 namespace JPMorrow.Revit.Tools.ConduitFittings
 {
 	// Represents a single fitting in a conduit run
-	readonly public struct Fitting
+	public class Fitting
 	{
 		public double Angle { get; }
 		public double Diameter { get; }
 		public string Type { get; }
 
-		public string GetAngleString(ModelInfo info) => RMeasure.AngleFromDouble(info, Angle);
-		public string GetDiameterString(ModelInfo info) => RMeasure.LengthFromDbl(info, Diameter);
+		public string GetAngleString(ModelInfo info) => RMeasure.AngleFromDouble(info.DOC, Angle);
+		public string GetDiameterString(ModelInfo info) => RMeasure.LengthFromDbl(info.DOC, Diameter);
 
 		public static string[] FittingMaterialTypes { get; } = new string[5] { "EMT", "RMC", "RNC", "IMC", "PVC" };
 
@@ -46,8 +46,8 @@ namespace JPMorrow.Revit.Tools.ConduitFittings
 
 		public Fitting(ModelInfo info, string angle, string diameter, string type)
 		{
-			double cvt_ang = RMeasure.AngleDbl(info, angle);
-			double cvt_dia = RMeasure.LengthDbl(info, diameter);
+			double cvt_ang = RMeasure.AngleDbl(info.DOC, angle);
+			double cvt_dia = RMeasure.LengthDbl(info.DOC, diameter);
 
 			if(cvt_ang == -1 || cvt_dia == -1)
 			{
--- libs/Util/RevitMeasuremets.cs
+++ libs/Util/RevitMeasuremets.cs
@@ -1,44 +1,58 @@
+/*
+    Author: Justin Morrow
+    Date Created: 4/21/2021
+    Description: A Module that helps with Revit measurement conversions
+*/
+
 using Autodesk.Revit.DB;
-using JPMorrow.Revit.Documents;
 using JPMorrow.Revit.Text;
 
-namespace JPMorrow.Revit.Measurements {
-    public static class RMeasure {
+namespace JPMorrow.Revit.Measurements
+{
+    public static class RMeasure
+    {
 #if REVIT2017 || REVIT2018 || REVIT2019 || REVIT2020 // DisplayUnitType Depreciated
-        public static double LengthDbl(ModelInfo info, string cvt_str) {
-            bool s = UnitFormatUtils.TryParse(info.DOC.GetUnits(), UnitType.UT_Length, cvt_str, out double val);
+        public static double LengthDbl(Document doc, string cvt_str) {
+            bool s = UnitFormatUtils.TryParse(doc.GetUnits(), UnitType.UT_Length, cvt_str, out double val);
             return s ? val : -1;
         }
 
-        public static double AngleDbl(ModelInfo info, string angle_str) {
-            bool s = UnitFormatUtils.TryParse(info.DOC.GetUnits(), UnitType.UT_Angle, angle_str, out double val);
+        public static double AngleDbl(Document doc, string angle_str) {
+            bool s = UnitFormatUtils.TryParse(doc.GetUnits(), UnitType.UT_Angle, angle_str, out double val);
             return s ? val : -1;
         }
 
-        public static string LengthFromDbl(ModelInfo info, double dbl) {
-            return UnitFormatUtils.Format(info.DOC.GetUnits(), UnitType.UT_Length, dbl, true, false, CustomFormatValue.FeetAndInches);
+        public static string LengthFromDbl(Document doc, double dbl, bool inches = false) {
+            var v = inches ? CustomFormatValue.Inches : CustomFormatValue.FeetAndInches;
+            return UnitFormatUtils.Format(doc.GetUnits(), UnitType.UT_Length, dbl, true, false, v);
         }
 
-        public static string AngleFromDouble(ModelInfo info, double dbl) {
-            return UnitFormatUtils.Format(info.DOC.GetUnits(), UnitType.UT_Angle, dbl, true, false, CustomFormatValue.Angle);
-        }    
+        public static string AngleFromDouble(Document doc, double dbl) {
+            return UnitFormatUtils.Format(doc.GetUnits(), UnitType.UT_Angle, dbl, true, false, CustomFormatValue.Angle);
+        }  
+          
 #else // ForgeTypeId updated
-        public static double LengthDbl(ModelInfo info, string cvt_str) {
-            bool s = UnitFormatUtils.TryParse(info.DOC.GetUnits(), UnitTypeId.FeetFractionalInches, cvt_str, out double val);
-            return s ? val : -1;
-        }
-        
-        public static double AngleDbl(ModelInfo info, string angle_str) {
-            bool s = UnitFormatUtils.TryParse(info.DOC.GetUnits(), UnitTypeId.Degrees, angle_str, out double val);
+        public static double LengthDbl(Document doc, string cvt_str)
+        {
+            bool s = UnitFormatUtils.TryParse(doc.GetUnits(), SpecTypeId.Length, cvt_str, out double val);
             return s ? val : -1;
         }
 
-        public static string LengthFromDbl(ModelInfo info, double dbl) {
-            return UnitFormatUtils.Format(info.DOC.GetUnits(), SpecTypeId.Length, dbl, false, CustomFormatValue.FeetAndInches);
+        public static double AngleDbl(Document doc, string angle_str)
+        {
+            bool s = UnitFormatUtils.TryParse(doc.GetUnits(), SpecTypeId.Angle, angle_str, out double val);
+            return s ? val : -1;
         }
 
-        public static string AngleFromDouble(ModelInfo info, double dbl) {
-            return UnitFormatUtils.Format(info.DOC.GetUnits(), SpecTypeId.Angle, dbl, false, CustomFormatValue.Angle);
+        public static string LengthFromDbl(Document doc, double dbl, bool inches = false)
+        {
+            var v = inches ? CustomFormatValue.Inches : CustomFormatValue.FeetAndInches;
+            return UnitFormatUtils.Format(doc.GetUnits(), SpecTypeId.Length, dbl, false, v);
+        }
+
+        public static string AngleFromDouble(Document doc, double dbl)
+        {
+            return UnitFormatUtils.Format(doc.GetUnits(), SpecTypeId.Angle, dbl, false, CustomFormatValue.Angle);
         }
 #endif
     }
